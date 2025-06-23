347e93241316df28fabaa15000648a852fa2534d
Exitanly <abnasrulit@mail.ru>
2025-06-23 14:12:15 +0300

    tk

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 import tkinter as tk
+from tk import messagebox
+from functools import partial
 
+class AuthWindow:
+    def __init__(self, root, on_auth_success):
+        self.root = root
+        self.on_auth_success = on_auth_success
+
+        self.aith_window = tk.Toplevel(root)
+        self.auth_window.title("Авторизация")
+        self.auth_window.geometry("300x200")
+        self.auth_window.resizable(False, False)
+
+        self.username = tk.StringVar()
+        self.passworg = tk.StringVar()
+
+        
